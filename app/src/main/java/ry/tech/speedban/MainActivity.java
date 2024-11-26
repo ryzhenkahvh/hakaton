a08c1b32da7e8e074167be17d55c58d9d5cd68cf
jxpix <wouldress@gmail.com>
2024-11-26 20:42:51 +0300

    Updating the visual part of the application

--- app/src/main/java/ry/tech/speedban/MainActivity.java
+++ app/src/main/java/ry/tech/speedban/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.PopupMenu;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); // После апд IDE перестало воркать
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
@@ -29,6 +32,18 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean onNavigationItemSelected(@NonNull MenuItem item) {
         String itemName = (String) item.getTitle();
+
+        for (int i = 0; i < bottomNavigationView.getMenu().size(); i++) {
+            MenuItem menuItem = bottomNavigationView.getMenu().getItem(i);
+            ViewGroup.LayoutParams layoutParams = bottomNavigationView.findViewById(menuItem.getItemId()).getLayoutParams();
+            layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+            menuItem.setCheckable(true);
+        }
+
+        // Анимашка увеличения ико в докбаре
+        ViewGroup.LayoutParams selectedLayoutParams = bottomNavigationView.findViewById(item.getItemId()).getLayoutParams();
+        selectedLayoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT;
+
         switch (itemName) {
             case "Учебник":
                 switchFragment(new StudyFragment());
